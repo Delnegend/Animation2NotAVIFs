7a1645461ed60de4990b27ebc9b119cbabb93201
Delnegend <kiennguyen19323@gmail.com>
2022-04-30 18:17:24 +0700

    update

--- Ani2NotAvifs.py
+++ Ani2NotAvifs.py
@@ -27,11 +27,11 @@ def main():
     for item in getFiles("."):
 
         file_name = item.replace("."+item.split('.')[-1], "")
-        ffmpeg = f'ffmpeg -y -i "{item}" -map v -strict -2 -vf "scale=-1:\'min(1080,ih)\'" -pix_fmt yuv420p10le -f yuv4mpegpipe {file_name}.y4m"'
+        ffmpeg = f'ffmpeg -y -i "{item}" -map v -strict -2 -vf "scale=-1:\'min(1080,ih)\'" -pix_fmt yuv420p10le -f yuv4mpegpipe "{file_name}.y4m"'
         # ffmpeg = f'ffmpeg -y -i {item} -an -vf "scale=-1:\'min(1080,ih)\'" -pix_fmt yuv420p10le -f yuv4mpegpipe {file_name}.y4m"'
-        svtav1 = f'SvtAv1EncApp.exe -i {file_name}.y4m --preset 8 --film-grain 2 --progress 5 -b {file_name}.ivf'
-        rav1e = f'rav1e.exe {file_name}.y4m -v --speed 4 -o {file_name}.ivf'
-        avif = f'ffmpeg.exe -i {file_name}.ivf -c copy -map 0 -brand avis -f mp4 ..\\output\\{file_name}.avif'
+        svtav1 = f'SvtAv1EncApp.exe -i "{file_name}.y4m" --preset 8 --film-grain 2 --progress 5 -b "{file_name}.ivf"'
+        rav1e = f'rav1e.exe "{file_name}.y4m" -v --speed 4 -o "{file_name}.ivf"'
+        avif = f'ffmpeg.exe -i "{file_name}.ivf" -c copy -map 0 -brand avis -f mp4 "..\\output\\{file_name}.avif"'
 
         print_sign(f'Converting {item} to y4m')
 
@@ -39,15 +39,15 @@ def main():
 
             print_sign(f'Converting {item} to ivf using SVT-AV1')
             if (subprocess.run(svtav1, shell=True).returncode != 0):
-                print_sign(
-                    f"Cannot convert {item} using SVT-AV1. Falling back to Rav1e")
+                print_sign(f"Cannot convert {item} using SVT-AV1. Falling back to Rav1e")
+                remove(f"{file_name}.ivf")
                 subprocess.run(rav1e, shell=True)
 
             print_sign(f'Repacking {item} to avif')
             if (subprocess.run(avif, shell=True).returncode == 0):
                 print_sign(
                     f'Convert done. Moving {item} to the output folder and cleaning up')
-                move(file_name+".avif", '../output/')
+                # move(file_name+".avif", '../output/')
                 for item in getFiles("."):
                     if (item.endswith(".y4m") or item.endswith(".ivf")):
                         remove(item)
